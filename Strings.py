adddcc6cc8a9681e7bcf9117bcc0673dc998f1eb
Denys <samtallak@gmail.com>
2025-02-18 20:17:54 +0100

    strings

---
+++ Strings.py
@@ -0,0 +1,11 @@
+name = "senior michaelle bocielli"
+
+print(name.title())
+
+print("\nHello\n\nHow are you doing?\n\nGreat to know\n")
+print("Message:\n\tMessage1\n\tMessage2\n\tMessage3\nEND")
+
+a = "......statium arcadium........"
+a = a.strip(".")
+a = a.strip()
+print(a.title())
