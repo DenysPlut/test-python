5a9482937825fa0f6e48a88ce1e6b2e4c611c703
Denys <samtallak@gmail.com>
2025-02-18 19:19:32 +0100

    initial commit

---
+++ Variables.py
@@ -0,0 +1,14 @@
+a = "Hello"
+b = 35
+
+print(a)
+print(b)
+
+f_name = "Denys"
+l_name = "Samoilenko"
+
+age = 35
+
+full_name = (f_name + " " + l_name)
+
+print(full_name + " " + str(age) + " " + "years old")
---
+++ helloworld.py
@@ -0,0 +1,2 @@
+print("Hello world from PyCharm")
+print("Line number2")
