e032a16434dbd927fa29c4bfc5d3a36b89d83f04
Denys <samtallak@gmail.com>
2025-03-12 14:34:07 +0100

    methods upgrade

--- classes.py
+++ classes.py
@@ -3,10 +3,11 @@ from heroofukraine import *
 #----------------------main------------------------------------------
 
 hero1 = HeroOfUkraine('Roman',5, 'on Donbass', 12 )
-hero2 = HeroOfUkraine('Viktor', 14, 'on Zaporizhzhya', 37)
+hero2 = HeroOfUkraine('Viktor', 34, 'on Zaporizhzhya', 37)
 
 hero1.show_heroOfUkraine()
 hero2.move_up()
-hero1.rank_up()
-hero1.set_rank(15)
+hero1.level_up()
 hero2.show_heroOfUkraine()
+hero2.set_rank(5)
+
--- heroofukraine.py
+++ heroofukraine.py
@@ -1,24 +1,33 @@
-
 class HeroOfUkraine():
 
-    def __init__(self,name,level,honor, rank):
-        """Initiate the hero"""
+    def __init__(self, name, level, honor, rank=10):
+        """Initiate the hero with a default rank of 10"""
         self.name = name
         self.level = level
         self.honor = honor
-        self.rank = rank
+        self.rank = rank  # Default rank set to 10 if not provided
 
     def show_heroOfUkraine(self):
         """Print all values"""
-        description = (self.name + ' is honored with ' + str(self.level) + ' medals for defending Ukraine against Russian conquerors ' + self.honor + ' and he ranked with ' + str(self.rank)).title() + ' medals in total'
+        description = (self.name + ' is honored with ' + str(self.level) +
+                       ' medals for defending Ukraine against Russian conquerors ' +
+                       self.honor + ' and he ranked with ' + str(self.rank)).title() + ' medals in total'
         print(description)
 
     def rank_up(self):
-        """Upgrade the level of Hero"""
+        """Upgrade the rank of Hero"""
         self.rank += 1
+
+    def level_up(self):
+        """Increase level by 3"""
+        self.level += 3
+        print(str(self.level) + ' highly endorsed')
 
     def move_up(self):
         """Moving"""
         print(self.name + ' is defending Ukraine from Russian aggressors')
+
     def set_rank(self, new_rank):
+        """Set a new rank"""
         self.rank = new_rank
+        print(str(self.rank) + ' promoted')
