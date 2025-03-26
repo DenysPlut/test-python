0ff8631f8749a20754d1e3794105ebc4a6945b32
Denys <samtallak@gmail.com>
2025-03-26 19:12:52 +0100

    Exit

--- Exceptions.py
+++ Exceptions.py
@@ -1,17 +1,22 @@
+import sys
 
 filename = "Lessons_List.txt"
 
-try:
-    print("Inside TRY")
-    myfile = open(filename, mode='r', encoding='Latin-1')
-except Exception:
-    print("Inside EXCEPT")
-    print("Error FOUND!")
-else:
-    print("Inside ELSE")
-    print(myfile.read())
-finally:
-    print("Inside FINALLY")
+while True:
 
-    print("============================EOF==============================")
+   try:
+       print("Inside TRY")
+       myfile = open(filename, mode='r', encoding='Latin-1')
+   except Exception:
+       print("Inside EXCEPT")
+       print("Error FOUND!")
+       filename = input("Enter correct filename! : ")
+   else:
+       print("Inside ELSE")
+       print(myfile.read())
+       sys.exit()
+   finally:
+       print("Inside FINALLY")
+
+       print("============================EOF==============================")
 
