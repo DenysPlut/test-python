5649db0e967e79697dfb30470e503480032046fd
Denys <samtallak@gmail.com>
2025-03-27 14:01:05 +0100

    regex

---
+++ RegEx.py
@@ -0,0 +1,8 @@
+import re
+
+mytext = "User123, 1995, user123@example.com, Germany" \
+         "User456, 1988, user456@mail.com, France" \
+         "User789, 2002, user789@domain.com, USA" \
+         "User999, 1975, user999@web.net, Canada"
+
+textlookfor = ""
