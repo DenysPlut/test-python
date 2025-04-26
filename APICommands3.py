1920f9ab61435808f9826217f68c36da8b43d3bd
Denys <samtallak@gmail.com>
2025-04-26 21:13:45 +0200

    download

--- APICommands3.py
+++ APICommands3.py
@@ -1,2 +1,16 @@
 from urllib import request, parse
-import sysч
+import sys
+
+myURL = "https://beejwala.com/cdn/shop/files/GladiolusRedColorFlowerBulb_1024x.jpg"
+myFile = "C:\\Users\\User\\Downloads\\myGladiolus.jpg"
+
+try:
+      print("Start downloading file from: " + myURL)
+      request.urlretrieve(myURL, myFile)
+
+except Exception:
+    print("Attention sys info")
+    print(sys.exc_info())
+    exit
+
+print("File downloaded and saved at: " + myFile )
